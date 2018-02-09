b62859c5df2d3405a542ee379355cee7ad37d50c
elawson668 <elawsont668@gmail.com>
2018-02-09 02:19:27 -0500

    update

--- hw1.cpp
+++ hw1.cpp
@@ -44,13 +44,79 @@ void handle_read_request(char* filename, int sd, struct sockaddr * client, sockl
 	else
 	{
 
-	char read_buf[512];
-	bzero(read_buf, 512);
+		char read_buf[512];
+		bzero(read_buf, 512);
+		uint16_t block=1;
+		while (true)
+		{
+			int bytes_read = fread(read_buf, 1, 512, file_to_read);
+			printf("%d bytes read from file\n", bytes_read);
+
+			if (bytes_read < 512)
+			{
+				printf("%d bytes left. This is last packet\n", bytes_read);
+				char last_pack[bytes_read+4];
+				bzero(last_pack, bytes_read);
+				char* last_ptr = last_pack;
+				uint16_t opcode = DATA;
+				opcode=htons(opcode);
+				block=htons(block);
+				memcpy(last_ptr, &opcode, sizeof(uint16_t));
+				last_ptr+=sizeof(uint16_t);
+				memcpy(last_ptr, &block, sizeof(uint16_t));
+				last_ptr+=sizeof(uint16_t);
+				memcpy(last_ptr, &read_buf, bytes_read);
+				sendto(sd, last_pack, sizeof(last_pack),0, client, *length);
+	
+				break;
+			}
+
+			else
+			{
+				char packet[516];
+				char* data_packet = packet;
+				uint16_t opcode = DATA;
+				uint16_t bl=htons(block);
+				opcode=htons(opcode);
+				memcpy(data_packet, &opcode, sizeof(uint16_t));
+				data_packet+=sizeof(uint16_t);
+				memcpy(data_packet, &bl, sizeof(uint16_t));
+				data_packet+=(sizeof(uint16_t));
+				memcpy(data_packet, &read_buf, sizeof(read_buf));
+	
+				sendto(sd, packet, sizeof(packet),0, client, *length);
+	
+
+
+				char ack_packet[4];
+				recvfrom( sd, ack_packet, 4, 0, client, length );
+				uint16_t op = ACK;
+				uint16_t inopcode = ack_packet[0] + ack_packet[1];
+				uint16_t inblock =  ntohs(ack_packet[2]) + ntohs(ack_packet[3]);
+				printf("%u ", inblock);
+				if (inopcode == op)
+				{printf("YAY\n");}
+				block++;
+			}
+
+		}
+
+	}	
+
+}
+
+/*
+void handle_write_request(char* filename, int sd, struct sockaddr * client, socklen_t* length)
+{	
+
+	char write_buf[512];
+	bzero(write_buf, 512);
 	uint16_t block=1;
 	while (true)
 	{
-		int bytes_read = fread(read_buf, 1, 512, file_to_read);
-		printf("%d bytes read from file\n", bytes_read);
+		int bytes_written = fread(written_buf, 1, 512, file_to_read);
+		printf("%d bytes written from file\n", bytes_read);
+
 		if (bytes_read < 512)
 		{
 			printf("%d bytes left. This is last packet\n", bytes_read);
@@ -99,15 +165,9 @@ void handle_read_request(char* filename, int sd, struct sockaddr * client, sockl
 		}
 
 	}
-
-	}
-
-
-
-	
 	
 
-}
+}*/
 
 
  
@@ -118,100 +178,104 @@ void handle_read_request(char* filename, int sd, struct sockaddr * client, sockl
 int main (int argc, char* argv[])
 {
 
-int sd; 
-struct sockaddr_in udpserver;
-int length = sizeof(udpserver);
+	int sd; 
+	struct sockaddr_in udpserver;
+	int length = sizeof(udpserver);
 
-sd = socket( AF_INET, SOCK_DGRAM, 0 ); 
+	sd = socket( AF_INET, SOCK_DGRAM, 0 ); 
 
-  if ( sd < 0 )  
-  {
-    perror( "socket() failed" );
+	if ( sd < 0 )  
+	{
+		perror( "socket() failed" );
     return EXIT_FAILURE;
-  }
+	}
 
-bzero(&udpserver, length);
-udpserver.sin_family = AF_INET;
-udpserver.sin_addr.s_addr = htonl(INADDR_ANY);
-udpserver.sin_port = htons(0); 
+	bzero(&udpserver, length);
+	udpserver.sin_family = AF_INET;
+	udpserver.sin_addr.s_addr = htonl(INADDR_ANY);
+	udpserver.sin_port = htons(0); 
 
-if ( bind( sd, (struct sockaddr *) &udpserver, length ) < 0 )
+	if ( bind( sd, (struct sockaddr *) &udpserver, length ) < 0 )
   {
-    perror( "bind() failed" );
-    return EXIT_FAILURE;
+   	perror( "bind() failed" );
+   	return EXIT_FAILURE;
   }
 
 
   if ( getsockname( sd, (struct sockaddr *) &udpserver, (socklen_t *) &length ) < 0 )
-  {
-    perror( "getsockname() failed" );
-    return EXIT_FAILURE;
-  }
+ 	{
+   	perror( "getsockname() failed" );
+   	return EXIT_FAILURE;
+ 	}
 
-  printf( "TFTP server assigned to port number %d\n", ntohs( udpserver.sin_port ) );
+ 	printf( "TFTP server assigned to port number %d\n", ntohs( udpserver.sin_port ) );
 
 
- struct sockaddr_in client;
-  int leng = sizeof( client );
+	struct sockaddr_in client;
+	int leng = sizeof( client );
 
-int pid;
-while (1)
-{
+	int pid;
+	while (1)
+	{
 	
-	char buffer[1024];
-	bzero(buffer,1024);
-	int bytes_read = recvfrom( sd, buffer, 1024, 0, (struct sockaddr *) &client,
+		char buffer[1024];
+		bzero(buffer,1024);
+		int bytes_read = recvfrom( sd, buffer, 1024, 0, (struct sockaddr *) &client,
                   (socklen_t *) &leng );
 
 	
-	if (bytes_read > 0)
-	{
-
-	
-		
-		
-		uint16_t opcode = buffer[0] + buffer[1];
-		if (opcode == 1)
-		{ 
-			
-		char readfile[1024];
-		for (int i=2; i < 1024; i++)
+		if (bytes_read > 0)
 		{
-			if (buffer[i] == '\0') 
-			{
 
-				readfile[i-2] = '\0';
-				handle_read_request(readfile, sd, (struct sockaddr *) &client, (socklen_t *) &leng);
-				break;
-
-			}
-
-			else
-			{
-				readfile[i-2] = buffer[i];
-
-			}
+			uint16_t opcode = buffer[0] + buffer[1];
+			if (opcode == 1)
+			{ 
 			
+				char readfile[1024];
+				for (int i=2; i < 1024; i++)
+				{
+					if (buffer[i] == '\0') 
+					{
 
+						readfile[i-2] = '\0';
+						handle_read_request(readfile, sd, (struct sockaddr *) &client, (socklen_t *) &leng);
+						break;
+
+					}	
+
+					else
+					{
+						readfile[i-2] = buffer[i];
+					}
+
+				}
+			}
+
+			if (opcode == 2) 
+			{
+
+				char writefile[1024];
+				for(int i=2; i < 1024; i++) 
+				{
+					if(buffer[i] == '\0')
+					{
+						writefile[i-2] = '\0';
+						//handle_write_request(writefile,sd,(struct sockaddr *) &client, (socklen_t *) &leng);
+						break;
+					}
+
+					else
+					{
+						writefile[i-2] = buffer[i];
+					}
+
+
+				}
+
+			}
 		}
-
-
-
-		}
-
-
-		
-	
-
 	}
-	
 
-}
-
-
-
-
-
-return 0;
+	return 0;
 
 }
