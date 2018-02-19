0cf8fd68b9f52f10306c7802c9e3f3a724ba56ed
elawson668 <elawsont668@gmail.com>
2018-02-18 23:06:55 -0500

    header file

--- hw1.h
+++ hw1.h
@@ -1,6 +1,6 @@
 void handle_alarm(int signum);
 
-void handle_error(int sd, struct sockaddr * client, socklen_t* length, uint16_t errcode, std::string message);
+//void handle_error(int sd, struct sockaddr * client, socklen_t* length, uint16_t errcode, std::string message);
 
 void handle_read_request(char* filename, int sd, struct sockaddr* client, socklen_t* length);
 
