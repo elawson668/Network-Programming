fbf926a8b6b45b86aef93c582717d629d6423f63
psychicsnowman <mohrm@rpi.edu>
2018-04-20 16:40:52 -0400

    it's a start
    start of loop

--- HW4/mpi_primes.c
+++ HW4/mpi_primes.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <signal.h>
+#include <math.h>
 
 int end_now = 0;
 
@@ -20,13 +21,29 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &count);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
-    
     signal(SIGUSR1, sig_handler);
-    
-    while (1) {
+    int n = id+1;
+    while (n < 10) {
         if (end_now == 1) {
             break;
         }
+	
+	int i;
+	int flag=0;
+	int prime=0;
+	int x =  sqrt(n);
+	for (i=2; i < x; i++)
+	{
+	if (n%i == 0)
+		{flag = 1;
+		break;}
+		 
+	}
+
+	if (flag == 0) {prime = 1;}
+	if (prime == 1) {printf("RANK%d %d\n", id, n);}
+	n= n + count;
+	
     }
     
     MPI_Finalize();
