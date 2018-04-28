2324bc7ba6698116f9a948c5bab08c014f559dd5
psychicsnowman <mohrm@rpi.edu>
2018-04-28 16:06:02 -0400

    heck if i know

--- HW4/mpi_primes.c
+++ HW4/mpi_primes.c
@@ -18,17 +18,20 @@ int main(int argc, char **argv)
 {
     int count, id;
     int local_primes=0;
-    int total_primes=1;
+    int total_primes=0;
     int total_end = 0;
-    int total_n;
-    
+    int total_n=0;
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &count);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     signal(SIGUSR1, sig_handler);
     int n = id + 2;
-    //while (n < 2147483647) {
-    while (n < 100){
+    int limit=10;
+    int narray[count];
+    int flags[count];
+    while (n < 2147483647) {
+    //while (n < 10){
 
 
 	//MPI_Allreduce(&end_now, &total_end, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
@@ -49,15 +52,30 @@ int main(int argc, char **argv)
 	}
 	
 	
+	int sendn=n;
+	int sendflag=flag;	
+	MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Gather(&sendn, 1, MPI_INT, narray, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Gather(&sendflag, 1, MPI_INT, flags, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	int j;
+	if (id==0)
+	{
+	for (j=0; j < count; j++)
+	{
+		if(flags[j] == 0) {total_primes++;}
+		if (narray[j] == limit) { printf("n: %d primes: %d\n", limit, total_primes); limit = limit*10;}
 
-	if (flag == 0) {local_primes++;}	
+	}
+	
 	n= n + count;
+
+	}
 	
     }
     
-    MPI_Allreduce(&n, &total_n, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
-    MPI_Allreduce(&local_primes, &total_primes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    if (id ==0) {printf("total_primes = %d\n", total_primes); printf("total_n = %d\n", total_n); }
+    //MPI_Allreduce(&n, &total_n, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+    //MPI_Allreduce(&local_primes, &total_primes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    //if (id ==0) {printf("total_primes = %d\n", total_primes); printf("total_n = %d\n", total_n); }
     //printf("RANK %d got to %d\n", id, n);
     MPI_Finalize();
 	
