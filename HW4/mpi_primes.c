04d9842b305abf0ab17a9e567a162e188d8ed0bf
psychicsnowman <mohrm@rpi.edu>
2018-04-28 15:05:19 -0400

    test
    all reduce

--- HW4/mpi_primes.c
+++ HW4/mpi_primes.c
@@ -17,39 +17,50 @@ void sig_handler(int signo)
 int main(int argc, char **argv)
 {
     int count, id;
+    int local_primes=0;
+    int total_primes=1;
+    int total_end = 0;
+    int total_n;
     
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &count);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     signal(SIGUSR1, sig_handler);
-
     int n = id + 2;
-    while (n<1000) {
+    //while (n < 2147483647) {
+    while (n < 100){
 
-        if (end_now == 1) {
-            break;
-        }
+
+	//MPI_Allreduce(&end_now, &total_end, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
 	
-		int i;
-		int flag = 0;
-		int prime = 0;
-		for (i = 2; i <= (int) sqrt(n); i++)
-		{
-			if (n % i == 0)
-			{
-				flag = 1;
-				break;
-			}
-		 
-		}
+	if (end_now == 1)
+	{
+		break;
+	}
 
-		if (flag == 0) {prime = 1;}
-		if (prime == 1) {printf("RANK%d %d\n", id, n);}
-		n = n + count;
+	int i;
+	int flag=0;
+	int x =  (int) sqrt(n);
+	for (i=2; i <= x; i++)
+	{	
+		if (end_now ==1) {flag=1; break;}
+		if (n%i != 0) {continue;}
+		else {flag=1; break;}
+	}
+	
+	
+
+	if (flag == 0) {local_primes++;}	
+	n= n + count;
 	
     }
     
+    MPI_Allreduce(&n, &total_n, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
+    MPI_Allreduce(&local_primes, &total_primes, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    if (id ==0) {printf("total_primes = %d\n", total_primes); printf("total_n = %d\n", total_n); }
+    //printf("RANK %d got to %d\n", id, n);
     MPI_Finalize();
+	
     
     return 0;
 }
